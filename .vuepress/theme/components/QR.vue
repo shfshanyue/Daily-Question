8af4c870aa07c680c8569754e57861a21724daa9
shfshanyue <xianger94@gmail.com>
2020-03-14 22:15:40 +0800

    update

--- .vuepress/theme/components/QR.vue
+++ .vuepress/theme/components/QR.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="qr">
-    <img src="http://mmbiz.qpic.cn/mmbiz_jpg/w7L7YMmnfhic7ooecF6PU5cGlHoakmTNBwKXf6VCC5TfAiagrmiahQ2kFxYLibPYgicicWsEgAFwsOrrOS16Bw2eml0w/0" alt="山月行" width="120" height="120" loading="lazy">
+    <img src="https://mmbiz.qpic.cn/mmbiz_jpg/w7L7YMmnfhic7ooecF6PU5cGlHoakmTNBwKXf6VCC5TfAiagrmiahQ2kFxYLibPYgicicWsEgAFwsOrrOS16Bw2eml0w/0" alt="山月行" width="120" height="120" loading="lazy">
     <p class="we-intro">
-      欢迎关注公众号<span class="we-highlight">全栈成长之路</span>。如果你是大厂招聘负责人，欢迎投递内推信息。如果你是候选人，将持续为你推送大厂内推机会及面经。
+      欢迎关注公众号<span class="we-highlight">互联网大厂招聘</span>。如果你是大厂招聘负责人，欢迎投递内推信息。如果你是候选人，将持续为你推送大厂内推机会及面经。
     </p>
   </div>
 </template>
